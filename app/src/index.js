1cf9901c8c319656c6bed71f9ae55a0afbeea018
Augusto Romero <augustoromerodev@gmail.com>
2022-06-13 01:20:32 -0300

    Methods updated

--- app/src/index.js
+++ app/src/index.js
@@ -20,28 +20,59 @@ const App = {
 
       // get accounts
       const accounts = await web3.eth.getAccounts();
+
       this.account = accounts[0];
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
   },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
+  setStatus: function(message,id) {
+    const status = document.getElementById(id);
     status.innerHTML = message;
   },
 
   createStar: async function() {
     const { createStar } = this.meta.methods;
     const name = document.getElementById("starName").value;
+    const symbol = document.getElementById("starSymbol").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
-    App.setStatus("New Star Owner is " + this.account + ".");
+    const result = await createStar(name,symbol, id).send({from: this.account});
+    console.log(result)
+    App.setStatus("New Star Owner is " + this.account + ".","status");
   },
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const lookId = document.getElementById("lookid").value;
+    let starInfo = await lookUptokenIdToStarInfo(lookId).call();
+    const {0: name, 1: symbol} = starInfo;
+    App.setStatus("Star info: Name:" + name+" Symbol:" + symbol, "status");
+  },
+
+  getOwner: async function (){
+    const { getOwnerStar } = this.meta.methods;
+    const lookId = document.getElementById("tokenid").value;
+    let starInfo = await getOwnerStar(lookId).call();
+    App.setStatus("Star owner:" + starInfo, "owner");
+  },
+
+  exchange: async function (){
+    const { exchangeStars } = this.meta.methods;
+    const lookId1 = document.getElementById("lookid1").value;
+    const lookId2 = document.getElementById("lookid2").value;
+    await exchangeStars(lookId1,lookId2).send({from: this.account});
+    App.setStatus("Exchange done", "message");
+  },
+
+  transfer: async function (){
+    const { transferStar } = this.meta.methods;
+    const addressTO = document.getElementById("addressto").value;
+    console.log(addressTO)
+    const tokenId = document.getElementById("tokenidto").value;
+    await transferStar(addressTO,tokenId).send({from: this.account});
+    App.setStatus("Transfer done", "messagetransfer");
   }
 
 };
@@ -52,7 +83,7 @@ window.addEventListener("load", async function() {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
+    await window.ethereum.request({ method: "eth_requestAccounts" }); // get permission to access accounts
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,10 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
+ //
+ // const fs = require('fs');
+ const mnemonic = "view nephew ribbon tone alpha clip smart hope mandate wish cabbage drum";
 
 module.exports = {
   /**
@@ -49,6 +48,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
+      gasPrice: 0,  // 20 gwei (in wei) (default: 100 gwei)
       network_id: "*",       // Any network (default: none)
     },
 
@@ -64,14 +64,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/088fc3da350c40d8b61fa46ee3b56116`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
@@ -83,13 +83,13 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    timeout: 200000
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: ">=0.7.0 <0.9.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
